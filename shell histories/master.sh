7e76150708b338adb64413641ca69a309b8431e6
Jonathon Reading <jonathonreading@gmail.com>
2024-08-31 13:36:58 -0600

    add some manifests and shell histories

---
+++ shell histories/master.sh
@@ -0,0 +1,51 @@
+ls
+cd /etc/network
+ls
+vi interfaces
+vi interfaces
+nano interfaces
+nano interfaces.d
+cd interfaces.d
+ls
+cd ..
+cat if-up.d
+cd if-up.d
+ls
+cat resolved
+exit
+apt-get install ifconfig
+curl -sfL https://get.k3s.io | sh -
+cd ~
+ls
+sudo apt update
+sudo apt install curl
+curl -sfL https://get.k3s.io | sh -
+sudo iptables -F sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo iptables -F
+sudo apt update
+sudo apt upgrade
+sudo apt install iptables
+sudo iptables -F
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo update-alternatives --set iptables /usr/sbin/ip6tables-legacy
+sudo update-alternatives --set iptables /usr/sbin/ip6tables-legacy
+sudo reboot
+cd ~
+cd ~
+curl -sfL https://get.k3s.io | sh -
+kubectl get node
+cat /var/lib/rancher/k3s/server/node-token
+cd ~
+kubectl get nodes
+
+iptables
+sudo apt update
+sudo papt upgrade
+sudo apt upgrade
+sudo apt install iptables
+sudo iptables -F
+kubectl get nodes
+kubectl get nodes
+kubectl get nodes
+kubectl get nodes
+exit
---
+++ shell histories/workerb.sh
@@ -0,0 +1,37 @@
+ls
+cd /etc/network
+ls
+vi interfaces
+vi interfaces
+nano interfaces
+nano interfaces.d
+cd interfaces.d
+ls
+cd ..
+cat if-up.d
+cd if-up.d
+ls
+cat resolved
+exit
+apt-get install ifconfig
+curl -sfL https://get.k3s.io | sh -
+cd ~
+ls
+sudo apt update
+sudo apt install curl
+curl -sfL https://get.k3s.io | sh -
+sudo iptables -F sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo iptables -F
+sudo apt update
+sudo apt upgrade
+sudo apt install iptables
+sudo iptables -F
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo update-alternatives --set iptables /usr/sbin/ip6tables-legacy
+sudo update-alternatives --set iptables /usr/sbin/ip6tables-legacy
+sudo reboot
+cd ~
+cd ~
+clear
+curl -sfL https://get.k3s.io | K3S_TOKEN="REDACTED" K3S_URL="https://REDACTED:6443" K3S_NODE_NAME="k3s-worker-b" sh -
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
