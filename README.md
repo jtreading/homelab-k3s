4a34ef13c05a6354b3b2419100493ded55b34518
Jonathon Reading <jonathonreading@gmail.com>
2024-08-31 13:44:58 -0600

    update master.sh with minecraft stuff, update readme.md

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # homelab-k3s
-A collection of miscellaneous scripts, commands, etc. used in setting up my homelab k3s cluster
+
+A collection of miscellaneous scripts, commands, etc., used in setting up my homelab k3s cluster.
+
+## Steps Taken So Far
+
+1. **Install Proxmox VE on Supermicro Server**
+    - There's a hardware RAID controller configured with one drive in RAID0.
+    - Proxmox indicates that this should rule out ZFS, but it still works.
+
+2. **Download Latest Debian ISO and Add to Proxmox**
+
+3. **Create a Virtual Machine Template**
+    - Include needed utilities, IP configuration, and k3s (note: it may be better not to include k3s in the template).
+
+4. **Clone the Template**
+    - Create one master node and two worker nodes.
+
+5. **Utilize Manifests and Helm**
+    - Installed on the master node.
+    - Create k3s pods, NodePorts, etc., to enable testing services.
+
+## Resources Used
+
+- [Proxmox VE Installation Guide](https://www.youtube.com/watch?v=X9fSMGkjtug)
+- [How to Set Up k3s on a Raspberry Pi 4](https://dev.to/jhqcat/how-to-set-up-k3s-on-a-raspberry-pi-4-4343)
+- [Helm Installation Documentation](https://helm.sh/docs/helm/helm_install/)
+- [Kubectl Quick Reference](https://kubernetes.io/docs/reference/kubectl/quick-reference/)
--- shell histories/master.sh
+++ shell histories/master.sh
@@ -49,3 +49,122 @@ kubectl get nodes
 kubectl get nodes
 kubectl get nodes
 exit
+
+#######
+
+ls
+cd /etc/network
+ls
+vi interfaces
+vi interfaces
+nano interfaces
+nano interfaces.d
+cd interfaces.d
+ls
+cd ..
+cat if-up.d
+cd if-up.d
+ls
+cat resolved
+exit
+apt-get install ifconfig
+curl -sfL https://get.k3s.io | sh -
+cd ~
+ls
+sudo apt update
+sudo apt install curl
+curl -sfL https://get.k3s.io | sh -
+sudo iptables -F sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo iptables -F
+sudo apt update
+sudo apt upgrade
+sudo apt install iptables
+sudo iptables -F
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo update-alternatives --set iptables /usr/sbin/ip6tables-legacy
+sudo update-alternatives --set iptables /usr/sbin/ip6tables-legacy
+sudo reboot
+cd ~
+cd ~
+curl -sfL https://get.k3s.io | sh -
+kubectl get node
+cat /var/lib/rancher/k3s/server/node-token
+cd ~
+kubectl get nodes
+
+iptables
+sudo apt update
+sudo papt upgrade
+sudo apt upgrade
+sudo apt install iptables
+sudo iptables -F
+kubectl get nodes
+kubectl get nodes
+kubectl get nodes
+kubectl get nodes
+exit
+cd ~
+ls
+kubectl get nodes
+kubectl get nodes
+kubectl get nodes
+cat ~/.bash_history
+ping 10.0.0.1
+ls
+nano manifest.yaml
+kubectl apply -f manifest.yaml
+kubectl get nodes
+kubectl get pods
+kubectl get pods -o wide
+kubectl get pods -o wide
+kubectl get pods -o wide
+nano np-manifest.yaml
+kubectl apply -f np-manifest.yaml
+nano np-manifest.yaml
+kubectl apply -f np-manifest.yaml
+kubectl get services
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+helm --help
+helm repo add itzg https://itzg.github.io/minecraft-server-charts/
+helm search repo itzg
+helm search repo itzg
+helm install --name 4.22.2 itzg/minecraft
+helm install itzg/minecraft
+helm install --generate-name itzg/minecraft
+helm install minecraft   --set minecraftServer.eula=true itzg/minecraft
+kubectl config view --raw >~/.kube/config
+helm install minecraft   --set minecraftServer.eula=true itzg/minecraft
+export POD_NAME=$(kubectl get pods \
+    --namespace default \
+    -l "component=minecraft-minecraft" \
+    -o jsonpath="{.items[0].metadata.name}")
+kubectl get pods
+kubectl get pods
+nano np-manifest.yaml
+kubectl get services
+kubectl get nodes
+kubectl get apps
+kubectl get pods
+nano np-manifest.yaml
+kubectl get namespaces
+kubectl get pds
+kubectl get pods
+kubectl get pods
+kubectl port-forward minecraft-minecraft-7bf7fb8b75-8b2td 25565:25565
+kubectl port-forward minecraft-minecraft-7bf7fb8b75-8b2td 25565:25565
+kubectl port-forward minecraft-minecraft-7bf7fb8b75-8b2td 25565:25565
+kubectl get pods
+ls
+nano mc-manifest.yaml
+kubectl apply -f mc-manifest.yaml
+nano mc-manifest.yaml
+kubectl apply -f mc-manifest.yaml
+kubectl get services
+nano mc-manifest.yaml
+kubectl apply -f mc-manifest.yaml
+nano mc-manifest.yaml
+kubectl apply -f mc-manifest.yaml
+kubectl get pods
+kubectl get pods -o wide
+ls
+exit
